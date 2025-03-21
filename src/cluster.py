c5a68f5bf7f435460d81b52b50c184981d2207fe
BobZhang <39358787+zbw123456@users.noreply.github.com>
2025-03-22 00:26:52 +0100

    Create cluster.py

---
+++ src/cluster.py
@@ -0,0 +1,9 @@
+import argparse
+import os
+import pathlib
+import shutil
+from datetime import datetime
+from subprocess import Popen
+
+from cfgs.config import PROJECT_PATH
+from scripts.cluster_scripts.utils import Overrides
